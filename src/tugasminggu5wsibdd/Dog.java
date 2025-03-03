3f70e90a039612ac8678cbbd7464248645d5d7ae
DarisPutra <darisff2@gmail.com>
2025-03-03 22:23:31 +0700

    p

---
+++ src/tugasminggu5wsibdd/Dog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasminggu5wsibdd;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Dog extends Animal {
+    @Override
+    public void makeNoise() {
+        System.out.println("Bark");
+    }
+}
